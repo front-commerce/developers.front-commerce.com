2659ba486aec5fc39c0b87788503380b9afe8184
Adrien Louis-Rossignol <adrien@occitech.fr>
2018-06-20 18:30:14 +0200

    Replace heavy image by a lighter one

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -222,7 +222,7 @@ features much more easier.** Design Systems help to achieve this!
 The design system is also a the place where we will help you to ensure you do not introduce regressions
 and you keep good a11y practices.
 `,
-        image: imgUrl("design-system.png"),
+        image: imgUrl("design-system.jpg"),
         imageAlign: "left",
         title: "Design system"
       }
