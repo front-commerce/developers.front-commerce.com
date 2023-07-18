9d6831635b286bb085e2e8843fd8cddf29b9a528
Pierre Martin <pierre@front-commerce.com>
2023-07-18 12:39:58 +0200

    refactor: remove duplicated content generation logic in `<SinceVersion />`

--- src/components/SinceVersion.tsx
+++ src/components/SinceVersion.tsx
@@ -17,11 +17,5 @@ export default function SinceVersion(props: SinceVersionProps) {
     return <span>{content}</span>;
   }
 
-  return (
-    <p>
-      <span className="inline-flex items-center rounded-full bg-pink-100 dark:bg-pink-800/50 px-3 py-2 text-xs font-medium text-pink-800 dark:text-pink-200">
-        Since version {props.tag}
-      </span>
-    </p>
-  );
+  return <p>{content}</p>;
 }
