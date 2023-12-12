5cde2a7bb491dfe528026b2d2b99c335bf0153d4
Paul van Dyk <paul@vandyk.fr>
2023-12-12 15:58:36 +0100

    chore: add components to global MDX context

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,23 @@
+import React from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+
+import ContactLink from "@site/src/components/ContactLink";
+import SinceVersion from "@site/src/components/SinceVersion";
+import ChangelogFooter from "@site/src/components/ChangelogFooter";
+import Figure from "@site/src/components/Figure";
+import BrowserWindow from "@site/src/components/BrowserWindow";
+import Calendly from "@site/src/components/Calendly";
+import Description from "@site/src/components/Description";
+import BackportList from "@site/src/components/BackportList";
+
+export default {
+  ...MDXComponents,
+  ContactLink,
+  SinceVersion,
+  ChangelogFooter,
+  Figure,
+  BrowserWindow,
+  Calendly,
+  Description,
+  BackportList,
+};
